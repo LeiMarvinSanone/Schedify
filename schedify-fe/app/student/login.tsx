27245bb0a664f7de98c9d27eb9e908e9e7ed3f17
Lei Marvin <sanoneleimarvin@gmail.com>
2026-04-11 10:28:56 +0800

    fix: force Google account picker on login (BUG-02)

--- schedify-fe/app/student/login.tsx
+++ schedify-fe/app/student/login.tsx
@@ -60,6 +60,7 @@ const Login = () => {
   async function handleGoogleSignIn() {
     try {
       await GoogleSignin.hasPlayServices();
+      await GoogleSignin.signOut();
       const userInfo = await GoogleSignin.signIn();
       const idToken = userInfo.data?.idToken;
 
